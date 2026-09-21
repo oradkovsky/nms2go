30b550e6496b124cb0f63af00525373bf1af9123
oradkovsky <ostap.radkovskyy@gmail.com>
2026-09-21 10:50:15 +0300

    introduce vm to overview screen

--- app/src/main/java/com/ror/nms2go/ui/navigation/Nms2GoNavHost.kt
+++ app/src/main/java/com/ror/nms2go/ui/navigation/Nms2GoNavHost.kt
@@ -22,6 +22,7 @@ import com.ror.nms2go.ui.ConfigScreen
 import com.ror.nms2go.ui.OrderDetailScreen
 import com.ror.nms2go.ui.OrdersHistoryScreen
 import com.ror.nms2go.ui.OverviewScreen
+import com.ror.nms2go.ui.OverviewViewModel
 import com.ror.nms2go.ui.ParsedScreen
 import com.ror.nms2go.ui.ParsedViewModel
 import com.ror.nms2go.ui.QrScanScreen
@@ -59,13 +60,21 @@ fun Nms2GoNavHost(
         popExitTransition = { slideOutBack() }
     ) {
         composable(Destinations.OVERVIEW) {
+            val overviewViewModel: OverviewViewModel =
+                androidx.lifecycle.viewmodel.compose.viewModel()
+            val overviewUiModel by overviewViewModel.uiModel.collectAsState()
+            LaunchedEffect(senders, loading, statusText, overviewResults) {
+                overviewViewModel.updateData(senders, loading, statusText, overviewResults)
+            }
+            LaunchedEffect(overviewViewModel) {
+                overviewViewModel.loadRequests.collect { onLoad() }
+            }
+            LaunchedEffect(overviewViewModel) {
+                overviewViewModel.parseItemRequests.collect(onParseItem)
+            }
             OverviewScreen(
-                senders = senders,
-                loading = loading,
-                statusText = statusText,
-                overviewResults = overviewResults,
-                onLoad = onLoad,
-                onParseItem = onParseItem,
+                uiModel = overviewUiModel,
+                onParseItem = overviewViewModel::onItemClicked,
                 onNavigateToConfig = {
                     navController.navigate(Destinations.CONFIG) {
                         popUpTo(navController.graph.findStartDestination().id) {
